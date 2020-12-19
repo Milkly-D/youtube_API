6342b9720acec650c1f5ef8181d2124bdfbdc7fd
milkly <ikakarasu714@gmail.com>
2020-12-19 21:05:01 +0900

    first commit

---
+++ youtube_api.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from apiclient.discovery import build
+
+APY_KEY = 'AIzaSyDz_828wHuTU8w0vZ67nFbAA0zjqckHiTQ'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+SEARCH_TEXT ='Nintendo'
+
+channel_list = []
+video_list = []
+index = 0
+
+youtube = build(
+    YOUTUBE_API_SERVICE_NAME,
+    YOUTUBE_API_VERSION,
+    developerKey = APY_KEY
+)
+
+
+def getChannel():
+    global index
+    response = youtube.search().list(q=SEARCH_TEXT, part='id,snippet', maxResults=10).execute()
+
+    for item in response.get('items', []):
+        if item['id']['kind'] != 'youtube#channel':
+            continue
+        channel_dict = {'index':str(index),'title':item['snippet']['title'],'channelId':item['snippet']['channelId']}
+        channel_list.append(channel_dict)
+        index += 1
+    print(channel_list)
+
+getChannel()
